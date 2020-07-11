6e64f6fe10bd9ff139aba8cd54a55d86d6f7cc24
Barton NIU <barton.niu@gmail.com>
2020-07-11 20:56:46 +1000

    Create the first blog

---
+++ blog/2020-07-11-Thinking-about-CORS.md
@@ -0,0 +1,29 @@
+---
+title: Thinking about CORS
+author: Ruihua
+description: In-depth understanding of CORS
+image: /img/cover.jpg
+tags: [IT, Front end, HTTP, CORS]
+---
+
+import useBaseUrl from '@docusaurus/useBaseUrl';
+
+<img alt="cover image" src={useBaseUrl('img/cover.jpg')} />
+
+<!-- ![Cover image](/img/cover.jpg) -->
+
+Since I have met the CORS issues for a while and this time I met it during the real production project development.
+
+I got to do some research to make it clear.
+
+#### Things below need to be cleared:
+
+- What is CORS?
+- Why there is CORS?
+- How to handle with CORS?
+
+<!-- truncate -->
+
+## What's CORS?
+
+According to the definition,
---
+++ docusaurus.config.js
@@ -0,0 +1,106 @@
+module.exports = {
+  title: 'My Site',
+  tagline: 'The tagline of my site',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  favicon: 'img/favicon.ico',
+  organizationName: 'Ruihua', // Usually your GitHub org/user name.
+  projectName: 'Rui Blog', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Rui Blog',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      links: [
+        // {
+        //   to: 'docs/',
+        //   activeBasePath: 'docs',
+        //   label: 'Docs',
+        //   position: 'left',
+        // },
+        // { to: 'blog', label: 'Blog', position: 'left' },
+        {
+          href: 'https://github.com/bartonniu',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      // links: [
+      //   {
+      //     title: 'Docs',
+      //     items: [
+      //       {
+      //         label: 'Style Guide',
+      //         to: 'docs/',
+      //       },
+      //       {
+      //         label: 'Second Doc',
+      //         to: 'docs/doc2/',
+      //       },
+      //     ],
+      //   },
+      //   {
+      //     title: 'Community',
+      //     items: [
+      //       {
+      //         label: 'Stack Overflow',
+      //         href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+      //       },
+      //       {
+      //         label: 'Discord',
+      //         href: 'https://discordapp.com/invite/docusaurus',
+      //       },
+      //       {
+      //         label: 'Twitter',
+      //         href: 'https://twitter.com/docusaurus',
+      //       },
+      //     ],
+      //   },
+      //   {
+      //     title: 'More',
+      //     items: [
+      //       {
+      //         label: 'Blog',
+      //         to: 'blog',
+      //       },
+      //       {
+      //         label: 'GitHub',
+      //         href: 'https://github.com/bartonniu',
+      //       },
+      //     ],
+      //   },
+      // ],
+      copyright: `Copyright © ${new Date().getFullYear()} Rui Blog All Rights Reserved.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          // It is recommended to set document id as docs home page (`docs/` path).
+          homePageId: 'doc1',
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          routeBasePath: '/',
+          // Please change this to your repo.
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
